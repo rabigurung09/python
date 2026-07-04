0395f37cd8d98be6d545cc9b50f0ceaee18fa07d
rabigurung09 <rabi72732@gmail.com>
2026-07-04 14:43:11 +0545

    update_2

---
+++ pratice/for_while.py
@@ -0,0 +1,46 @@
+name = ["ram","shyam"]
+y = 8
+for a in name:
+    print(a)
+# output=ram
+# shyam
+
+
+defn =["color_name"]
+color = ["red","yellow","orange"]
+for x in defn:
+    for y in color:
+        print(x,y)
+# output=color_name red
+# color_name yellow
+# color_name orange
+
+
+name = ["ram","shyam","hari"]
+for g in name :
+    print(g)
+    if g == "shyam":
+     break
+# oytput=ram
+# shyam
+for j in range(9):
+    print(j)
+# output=0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+college = ["universal","xavier","texas"]
+for d in college:
+    if d == "xavier":
+        continue
+    print(d)
+# output= universal
+# texas
+  
+
---
+++ pratice/while.py
@@ -0,0 +1,42 @@
+# i = 10
+# while i <  20 :
+#     print(i)
+#     i+=1
+# output=10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+i = 1
+while i < 12 :
+    i+=1
+    if i== 6:
+        continue
+    print(i)
+# output=2
+# 3
+# 4
+# 5
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+    
+    
+    
+i = 1
+while i < 12 :
+    print(i)
+    if (i==6):
+        break
+    i+=1
+# output=1
+# c
+ 
