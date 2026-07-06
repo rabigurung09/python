00db4ed6c4de887e43f54b5de812a71aa6575c4e
rabigurung09 <rabi72732@gmail.com>
2026-07-06 15:56:58 +0545

    update_4

---
+++ function/function_intro.py
@@ -0,0 +1,47 @@
+"""function: a function is a resuable blockof code that perform a specific task
+# it is defined using def key word and can be called whenever need, which helps to avoid writing same code multiple time
+
+"""
+# syntax:
+def function_name():#function defien
+    pass       #body
+function_name()#function call
+"""advantage:
+# it reduce duplication and rewrite multiple code at multiple time
+# we can solve errors easily inside the function because of define function that is easy to sport
+# 
+#types of function
+# 1 . inbuild function: it is predefine function that is in python which can b used freely.eg:pop(),print().
+# 2 . user_define function : it is a function that is define by user according there need is known as user_define function.
+# syntax:
+def function_name():#function defien
+    pass       #body
+function_name()#function call
+# 3 .
+# """
+# example_1:calling function
+def rabi():
+    print("hello rabi")
+rabi()
+# output: hello rabi
+
+
+
+# example_2: passing patrameter and argument
+def add(a,b):
+    print(a+b)
+add(3,8)
+# output= 11
+
+
+
+# example : area of rectangle
+def area(L,b):
+    print(L*b)
+area(10,50)
+# output: 500
+
+
+
+
+
