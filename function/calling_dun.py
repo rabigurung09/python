3aace74cc1bdc82051347324ce4bd484c45851a9
rabigurung09 <rabi72732@gmail.com>
2026-07-07 16:00:22 +0545

    update_5

---
+++ function/calling_dun.py
@@ -0,0 +1,15 @@
+# example1: calling function:
+def greet():
+    print("welcome to python class")
+greet()
+# output :welcome to python class
+
+
+# example 2: passing parameter and argument
+def add(a,b):
+    print(a+b)
+add(2,2)
+
+
+# 3 lambda function:
+# 4 recrusive function:
---
+++ function/lamda.py
@@ -0,0 +1,31 @@
+"""=====lamda function ====
+# a lamda function is small ,anonymous function that does not have name 
+
+# we use "lamda" keyword to define lamda function.
+
+# syntax
+lamda parameter :expression
+
+
+here,
+parameter ==> one or more
+but,
+expression ==> only one 
+this expression is execute (runs) and return result
+
+
+"""
+# explain 
+result = lambda a,b:a+b
+print(result(2,4))
+# output=6
+
+# area of rect
+output = lambda l,b:l*b
+print("the area of rectangle is : ",output(3,7))
+# output=the area of rectangle is :  21
+
+# area of square:
+output_1 =lambda a:a**2
+print("the area of square :",output_1(12))
+# output:the area of rectangle is :  21
